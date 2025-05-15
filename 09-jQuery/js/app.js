c82c33c396589df77f3721ab82b32ac36ed312c8
MariaFernanda563 <mariafernandag1234@gmail.com>
2025-05-15 17:35:03 -0500

    jQuery

--- 09-jQuery/js/app.js
+++ 09-jQuery/js/app.js
@@ -1,94 +1,68 @@
-$(function () {
-<<<<<<< HEAD
+$(function(){
 
-    // check localstorage
-    if (localStorage.getItem('todolist') != null) {
+    //Ckeck localStorage
+    if(localStorage.getItem('todolist') != null){
         $('.list').html(localStorage.getItem('todolist'))
         countTasks()
         countRemains()
+
     } else {
-        //count tasks & remains
+        //count Tasks and Remains
         countTasks()
         countRemains()
     }
-    //add tasks & remains
 
-=======
-    //count tasks & remains
-    countTasks()
-    countRemains()
-    //add tasks & remains
->>>>>>> 9f35df8fc11a2f962d9ef9f77594e215bad4cf20
-    $('footer').on('click', '#add', function () {
-        if ($('#input-task').val().length > 0) {
-
+    //add tasks
+    $('footer').on('click', '#add', function(){
+        if($('#input-task').val().length > 0){
+            
             $task = '<article> \
-                    <input type="checkbox"> \
-                    <p>'+ $('#input-task').val() + '</p> \
-                    <button>&times;</button> \
-                </article>'
-
+                        <input type="checkbox">\
+                        <p>'+$('#input-task').val()+'</p>\
+                        <button>&times;</button>\
+                    </article>'
             $('section.list').append($task)
             $('#input-task').val('')
             countTasks()
             countRemains()
-
         } else {
-            alert('Please! enter a task')
+            alert('Please! Enter a Task');
         }
     })
-    // toggle task (remain/done)
-    $('body').on('click', 'input[type=checkbox]', function () {
+    //Toggle Task (Remain or Done)
+    $('body').on('click', 'input[type="checkbox"]', function(){
         //if checked
-        if ($(this).prop('checked')) {
-<<<<<<< HEAD
+        if ($(this).prop('checked')){
             $(this).attr('checked', true)
             $(this).parent().addClass('checked')
-        } else {
+        }else{
             $(this).attr('checked', false)
-=======
-            $(this).parent().addClass('checked')
-        } else {
->>>>>>> 9f35df8fc11a2f962d9ef9f77594e215bad4cf20
             $(this).parent().removeClass('checked')
         }
         countRemains()
     })
-    //remove taks
-    $('body').on('click', 'article button', function () {
+    //Remove Task
+    $('body').on('click', 'article button', function() {
         $(this).closest('article').remove()
         countTasks()
         countRemains()
     })
+    $('body').on('click', '#reset', function(){
+        localStorage.setItem('todolist', $('.list').html(''))
+        countTasks()
+        countRemains()
+    })
 })
-
-function countTasks() {
+// Count Tasks
+function countTasks(){
     $('.num-tasks').text($('article').length)
-    $('.title-tasks').text($('article').length > 1 ? 'tasks' : 'task')
+    $('.title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
 }
-
-//count remains
-
-function countRemains() {
+//count Remains
+function countRemains(){
     $remain = Math.abs($('.checked').length - $('article').length)
     $('.num-remains').text($remain)
-    $('.title-remains').text($remain > 1 ? 'remains' : 'remain')
-<<<<<<< HEAD
-    //set localStorage
+    $('.title-remains').text($remain >1 ? 'Remains' : 'Remain')
+    // set localStorage
     localStorage.setItem('todolist', $('.list').html())
 }
-function reset(){
-    $('.list').html('')
-    localStorage.removeItem('todolist')
-    countTasks()
-    countRemains()
-}
-
-$('footer').on('click', '#reset', function(){
-    reset()
-})
-
-
-=======
-}
->>>>>>> 9f35df8fc11a2f962d9ef9f77594e215bad4cf20
